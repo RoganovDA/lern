1349602491b2170a1ce17b7b1b8673b6c553ce74
RoganovDA <slowrda@gmail.com>
2025-07-30 08:07:18 +0400

    fix build error by removing extendTheme

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import {
   Heading,
   Icon,
   Input,
-  Stack,
   Switch,
   Text,
   VStack,
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
 import App from './App.tsx';
 import './index.css';
 
-const theme = extendTheme({});
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ChakraProvider theme={theme}>
+    <ChakraProvider>
       <App />
     </ChakraProvider>
   </StrictMode>
